77a11c8befbfb5c94ac66db5b29c5ca26a66828f
tomschreck <tom@schreckmail.com>
2020-05-20 08:26:21 -0500

    implementation of elastic beanstalk

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,7 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
     // 1. validate the image_url query
     if (!image_url)
     {
-      return res.status(400).send(`image_url is a required query param.  ex: GET /filteredimage?image_url=abc.`);
+      return res.status(400).send(`image_url is a required query param.  ex: GET /filteredimage?image_url=https://images.pexels.com/photos/264279/pexels-photo-264279.jpeg`);
     }
 
     // 1. validate image_url is a valid url
@@ -42,7 +42,7 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
     try 
     {
-
+      // 2. call filterImageFromURL(image_url) to filter the image
       const filteredImage = await filterImageFromURL(image_url);
 
       if (filteredImage === "error")
@@ -51,33 +51,16 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
       }
       else
       {
+        // 3. send the resulting file in the response
+        // 4. deletes any files on the server on finish of the response
         res.status(200).sendFile(filteredImage, () => deleteLocalFiles([filteredImage]));
       }
     }
     catch (error) 
     {
-      res.status(415).send('URL is not an Image');
+      res.status(422).send('Image could not be processed');
     }
   });
-
-
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
 
 
 
