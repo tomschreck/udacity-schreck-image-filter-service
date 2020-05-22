f10d42421b571d5e0887b68e7587972b725c249a
tomschreck <tom@schreckmail.com>
2020-05-22 09:40:24 -0500

    fixes to align with requirements

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { ParsedQs } from 'qs';
 import bodyParser from 'body-parser';
 import isurl from 'is-url';
 import { filterImageFromURL, deleteLocalFiles } from './util/util';
@@ -17,16 +18,16 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
   // Root Endpoint
   // Displays a simple message to the user
-  app.get("/", async (req, res) =>
+  app.get("/", async (req: express.Request, res: express.Response) =>
   {
     res.send("try GET /filteredimage?image_url={{}}")
   });
 
 
   // test image:  https://images.pexels.com/photos/264279/pexels-photo-264279.jpeg
-  app.get("/filteredimage", async (req, res) =>
+  app.get("/filteredimage", async (req: express.Request, res: express.Response) =>
   {
-    let { image_url } = req.query;
+    const image_url: undefined | string | string[] | ParsedQs | ParsedQs[] = req.query.image_url;
 
     // 1. validate the image_url query
     if (!image_url)
@@ -35,30 +36,33 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
     }
 
     // 1. validate image_url is a valid url
-    if (!isurl(image_url))
+    else if (!isurl(image_url.toString()))
     {
       return res.status(400).send(`image_url value (${image_url}) is not a valid URL.`);
     }
 
-    try 
+    else
     {
-      // 2. call filterImageFromURL(image_url) to filter the image
-      const filteredImage = await filterImageFromURL(image_url);
+      try 
+      {
+        // 2. call filterImageFromURL(image_url) to filter the image
+        const filteredImage = await filterImageFromURL(image_url.toString());
 
-      if (filteredImage === "error")
-      {
-        res.status(415).send('URL is not an Image');
+        if (filteredImage === "error")
+        {
+          res.status(415).send('URL is not an Image');
+        }
+        else
+        {
+          // 3. send the resulting file in the response
+          // 4. deletes any files on the server on finish of the response
+          res.status(200).sendFile(filteredImage, () => deleteLocalFiles([filteredImage]));
+        }
       }
-      else
+      catch (error) 
       {
-        // 3. send the resulting file in the response
-        // 4. deletes any files on the server on finish of the response
-        res.status(200).sendFile(filteredImage, () => deleteLocalFiles([filteredImage]));
+        res.status(422).send('Image could not be processed');
       }
-    }
-    catch (error) 
-    {
-      res.status(422).send('Image could not be processed');
     }
   });
 
